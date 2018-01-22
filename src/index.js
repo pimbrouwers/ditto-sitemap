8f4fc8763b6178d33b7b7f980b50c7d8668493a9
Pim Brouwers <mpbrouwers@gmail.com>
2018-01-22 07:02:13 -0500

    console output removed

--- src/index.js
+++ src/index.js
@@ -3,73 +3,71 @@
  */
 
 const events = require('events'),
-    fs = require('fs-extra'),
-    path = require('path'),
-    sm = require('sitemap'),
-    util = require('util');
+  fs = require('fs-extra'),
+  path = require('path'),
+  sm = require('sitemap'),
+  util = require('util');
 
 module.exports = DittoSitemap;
 
 function DittoSitemap(opt) {
-    this.opt = opt || {
-        filename: "/sitemap.xml",
-        sitemapOptions: {
-            hostname: "http://mysite.com"
-        }
-    };
+  this.opt = opt || {
+    filename: "/sitemap.xml",
+    sitemapOptions: {
+      hostname: "http://mysite.com"
+    }
+  };
 
-    this.filename = null;
-    this.urls = [];
+  this.filename = null;
+  this.urls = [];
 };
 
 /* Inherit Event Emitter prototype */
 util.inherits(DittoSitemap, events.EventEmitter);
 
 DittoSitemap.prototype.run = function(files, Ditto, done) {
-    this.filename = path.resolve(Ditto._destination, this.opt.filename);
-    this.discoverUrls(files);
+  this.filename = path.resolve(Ditto._destination, this.opt.filename);
+  this.discoverUrls(files);
 
-    try {
-        //kickoff build
-        this.renderSitemap();
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+  try {
+    //kickoff build
+    this.renderSitemap();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 
-    //register listener
-    this.on("done", done);
+  //register listener
+  this.on("done", done);
 };
 
 DittoSitemap.prototype.discoverUrls = function(files) {
-    console.info("INFO: discoverUrls()");
-    let self = this;
+  let self = this;
 
-    Object.keys(files).forEach(function(filepath) {
-        let htmlFilePath = files[filepath].path,
-            url = "/"
+  Object.keys(files).forEach(function(filepath) {
+    let htmlFilePath = files[filepath].path,
+      url = "/"
 
-        if (htmlFilePath.indexOf("/index.html") >= 0) {
-            url = htmlFilePath.replace("/index.html", "");
-        }
+    if (htmlFilePath.indexOf("/index.html") >= 0) {
+      url = htmlFilePath.replace("/index.html", "");
+    }
 
-        self.urls.push({
-            url: url
-        });
+    self.urls.push({
+      url: url
     });
+  });
 };
 
 DittoSitemap.prototype.renderSitemap = function() {
-    console.info("INFO: renderSitemap()");
-    let self = this;
+  let self = this;
 
-    self.opt.sitemapOptions.urls = self.urls;
+  self.opt.sitemapOptions.urls = self.urls;
 
-    let sitemap = sm.createSitemap(self.opt.sitemapOptions);
+  let sitemap = sm.createSitemap(self.opt.sitemapOptions);
 
-    fs.outputFile(self.filename, sitemap.toString(), function(err) {
-        if (err) throw err;
+  fs.outputFile(self.filename, sitemap.toString(), function(err) {
+    if (err) throw err;
 
-        self.emit("done");
-    });
+    self.emit("done");
+  });
 };
